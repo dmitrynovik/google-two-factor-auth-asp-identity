421f603561f2bdfcc6c82441fd5afa218af15f06
dmitry.novik <dmitry.novik@vivant.com.au>
2017-04-26 16:49:34 +1000

    Added migrations

---
+++ AspNetGoogleAuth/Identity/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Diagnostics;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspNetGoogleAuth.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(): base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.Log = msg => Debug.WriteLine(msg);
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer(new ApplicationDbInitializer());            
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
