b13efd55b31ba0039567f2dbc5591b7e2fb7906d
dmitry.novik <dmitry.novik@vivant.com.au>
2017-04-26 16:18:02 +1000

    refactoring: class per file

---
+++ AspNetGoogleAuth/Identity/ApplicationUser.cs
@@ -0,0 +1,24 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspNetGoogleAuth.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public bool IsGoogleAuthenticatorEnabled { get; set; }
+
+        public string GoogleAuthenticatorSecretKey { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            //var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
